e9aba66f355819f40412c2df45446e2827e7cd0f
李泽骏 <lizejun@meizu.com>
2018-03-27 15:55:25 +0800

    [AttrDemo][添加例子]

--- app/src/main/java/com/demo/lizejun/attrdemo/AttrTextView.java
+++ app/src/main/java/com/demo/lizejun/attrdemo/AttrTextView.java
@@ -6,8 +6,9 @@ import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
+import android.widget.TextView;
 
-public class AttrTextView extends AppCompatTextView {
+public class AttrTextView extends TextView {
 
     public AttrTextView(Context context) {
         super(context);
@@ -15,25 +16,23 @@ public class AttrTextView extends AppCompatTextView {
 
     public AttrTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initAttr(context, attrs, 0);
+        initAttr(context, attrs);
     }
 
     public AttrTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initAttr(context, attrs, defStyleAttr);
+        initAttr(context, attrs);
     }
 
-    private void initAttr(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AttrTextView, defStyleAttr, 0);
+    private void initAttr(Context context, @Nullable AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AttrTextView, R.attr.AttrThemeStyle, 0);
         if (typedArray != null) {
-            typedArray = context.obtainStyledAttributes(attrs, R.styleable.AttrTextView);
-            String text = typedArray.getString(R.styleable.AttrTextView_attr_tv_name);
-            int color = typedArray.getColor(R.styleable.AttrTextView_attr_tv_color, 0);
+            String text = typedArray.getString(R.styleable.AttrTextView_attrTvName);
+            int color = typedArray.getColor(R.styleable.AttrTextView_attrTvColor, 0);
             setText(text);
             setTextColor(color);
             typedArray.recycle();
         }
     }
-
 
 }
